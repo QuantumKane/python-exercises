857e8ed1ee35c9f63acfaddd7e4925eb05f1d298
Steve Kane <quantumkane00@gmail.com>
2021-01-11 15:49:25 -0600

    done

--- python_introduction_exercises.py
+++ python_introduction_exercises.py
@@ -7,3 +7,5 @@ print("Hello World!")
 # 2. Inside of your hello world program, create a variable named greeting that contains the message that you will print to the console. 
 
 greeting = "Hello world! How ya doin?"
+
+print(greeting)
