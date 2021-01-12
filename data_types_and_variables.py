91bdfb869c84b1ef9a836fad3642f80800b9fe6a
Steve Kane <quantumkane00@gmail.com>
2021-01-12 17:02:57 -0600

    work in progress

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -4,24 +4,24 @@
 total_price = 0
 daily_price = 3.00
 
-l_mermaid = 3
-bro_bear = 5
-herc = 1
+l_mermaid_days = 3
+bro_bear_days = 5
+herc_days = 1
 
-total_price = daily_price * (l_mermaid + bro_bear + herc)
+total_price = daily_price * (l_mermaid_days + bro_bear_days + herc_days)
 
 # 2. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. How much will you receive in payment for this week? 
 
 weeks_pay = 0
-goog_pay = 400
-am_pay = 380
-face_pay = 350
+google_pay = 400
+amazon_pay = 380
+facebook_pay = 350
 
-goog_hours = 6
-am_hours = 4
-face_hours = 10
+google_hours = 6
+amazon_hours = 4
+facebook_hours = 10
 
-weeks_pay = (goog_pay * goog_hours) + (am_pay * am_hours) + (face_pay * face_hours)
+weeks_pay = (google_pay * google_hours) + (amazon_pay * amazon_hours) + (facebook_pay * facebook_hours)
 
 # 3. A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
 
@@ -29,28 +29,45 @@ is_not_full = True
 no_schedule_conflict = True                                                                                     
 
 if (is_not_full and no_schedule_conlict):
-    print("you can enroll")
+    print("You can enroll")
 else:
-    print("no go")
+    print("No go")
     
 # 4. A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
 
 has_not_expired = True
-two_or_more = True
+more_than_two = True
 prem_member = True
 
-if (two_or_more or prem_member) and has_not_expired;
+if (more_than_two or prem_member) and has_not_expired;
     print("You get the product offer")
 else:
-    print("maybe next time")
+    print("Maybe next time")
 
 # 5. Create a variable that holds a boolean value for each of the following conditions:
 # a. the password must be at least 5 characters
-at_least_five = True
+at_least_five = len(password) >= 5
 
 # b. the username must be no more than 20 characters
-max_twenty = True
+max_twenty = len(username) <= 20
 
 # c. the password must not be the same as the username
-pswd_same_as_user = False
+pswd_diff_from_user = password != username
+
+# LIST COMPREHENSION EXERCISES
+
+# 1. Make a variable named uppercased_fruits to hold the output
+uppercased_fruits = [fruit.upper() for fruit in fruits]
+
+# 2. Make a variable named capitalized_fruits
+capitalized_fruits = [fruit.title() for fruit in fruits]
+
+# 3. Make a variable named fruits_with_more_than_two_vowels
+v = ('a', 'e', 'i', 'o', 'u')
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if v > 2]
+
+# 5. Make a list that contains each fruit with more than 5 characters
+more_than_five = [len(fruit) for fruit in fruits]
+
+# 6. Make a list that contains each fruit with exactly 5 characters
 
